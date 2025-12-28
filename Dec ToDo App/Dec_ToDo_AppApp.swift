e2f247e821a8e2e00f098954ceff5dda1f60fde1
Miguel <mgamez20@icloud.com>
2025-12-27 20:36:47 -0500

    Initial Commit

---
+++ Dec ToDo App/Dec_ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dec_ToDo_AppApp.swift
+//  Dec ToDo App
+//
+//  Created by Miguel on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Dec_ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
