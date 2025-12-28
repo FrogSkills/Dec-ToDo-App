25159df8025fdab068305f75f02553ce6f893de4
Miguel <mgamez20@icloud.com>
2025-12-28 15:37:56 -0500

    Worked on the DetailView & added notes.

--- Dec ToDo App/ContentView.swift
+++ Dec ToDo App/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var subjects = ["Ues", "des"]
+    @State private var sheetIsShowing = false
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(subjects, id: \.self) { subject in
                     NavigationLink {
-                        DetailView(incomingWord: "EAT \(subject)")
+                        DetailView(incomingWord: "This is the incoming: \(subject)")
                             .foregroundStyle(.orange)
                     } label: {
                         Text("\(subject)")
@@ -23,6 +24,20 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Sure")
+            .sheet(isPresented: $sheetIsShowing) {
+                NavigationStack {
+                    DetailView(incomingWord: "")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsShowing.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
@@ -41,3 +56,19 @@ struct ContentView: View {
 // .navigationBarTitleDisplayMode(.-_-_-) right under.
 // .listStyle(.plain) right under.
 
+// . toolbar{
+//      ToolbarItem(placement: .topBarTrailing) {
+//          here we can insert a button if we want to (at the moment we always)
+
+
+// When adding data (plus) / creating,  the sheet comes from the bottom up, not the right.
+
+// add .sheet inside the NavigationStack, then add the window we want to popup (DetaiView window in this case) with the value that must be inputed.
+
+// .sheet(isPresented: $randomValue) {
+//      DetailView(incomingWord: "")
+//  }
+
+// There are no dismiss, cancel, or any buttons in the sheet. To show the ones we already have we need to embeed the DetailView(second window) (that is being called in the .sheet) in a NavigationStack.
+
+// instead of .sheet we can use .fullScreenCover
--- Dec ToDo App/DetailView.swift
+++ Dec ToDo App/DetailView.swift
@@ -8,22 +8,88 @@
 import SwiftUI
 
 struct DetailView: View {
-    var incomingWord: String
+    @State var incomingWord: String
+    @State private var reminderIsOn = false
+    @State private var dueDate = Date.now+(24*60*60)
+    @State private var notes = ""
+    @State private var isCompleted = false
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        Text("This is the DetailView")
-            .foregroundStyle(.black)
-        Text("The word of choice is \(incomingWord)")
-            .foregroundStyle(.black)
+        List{
+            TextField("Incoming text:", text: $incomingWord)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Set Reminder", isOn: $reminderIsOn)
+                .listRowSeparator(.hidden)
+            
+            DatePicker("Date", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .padding(.bottom)
+                .disabled(!reminderIsOn)
+            
+            Text("Notes:")
+                .padding(.top)
+            
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Has it been finished", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            
+        }
+        .listStyle(.plain)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //TODO: Save code
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView(incomingWord: "")
+    NavigationStack {
+        DetailView(incomingWord: "")
+    }
 }
 
 
-// create a var (not @state & not private) that has its value declared.
+// create a var (not private) that has its value declared.
 
-// DetailView(randomVarName: actualRandomValue)
+// DetailView(randomVarName: actualRandomValue) in the #Preview
 // place the declared var in the detail view and give it any random value.
+
+// @State private randomName = Date.now(24*60*60)
+
+
+// TextField("", text: $randomName, axis: .vertical)
+
+
+// See which views have axis.
+
+// .textFieldStyle(.roundedBorder)
+
+
+// .disabled()
+
+
+//  For a toolbar We do not need another NavigationStack since we inherit the one from the main page because this one is within that stack. BUT, we need to embed the #DetailView in the #Preview in a NavigationStack
+
+// . toolbar{
+//      ToolbarItem(placement: .topBarTrailing) {
+//          here we can insert a button if we want to (at the moment we always)
+
+
+// .navigationBarBackButtonHidden
