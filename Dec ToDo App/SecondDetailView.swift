82aaf90109d687ed2f73067325127a72ee187ab0
Miguel <mgamez20@icloud.com>
2025-12-28 10:54:34 -0500

    Now with updates!

---
+++ Dec ToDo App/SecondDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondDetailView.swift
+//  Dec ToDo App
+//
+//  Created by Miguel on 12/27/25.
+//
+
+import SwiftUI
+
+struct SecondDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondDetailView()
+}
